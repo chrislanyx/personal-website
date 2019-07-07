13704dcaae78eaaf13086d874c090eafe521c530
Li Wong Kin <wli455@gatech.edu>
2019-07-07 16:13:08 +0800

    Add scss style sheets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Wongkin Li
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Computer Science Student @ Georgia Tech
+        </p>
       </header>
     </div>
   );
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <p>This is a navbar.</p>
+    )
+  }
+}
+
+export default Navbar;
